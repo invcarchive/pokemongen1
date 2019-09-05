e9c9a1c3c352e4ffb3b3de0fe9cbc2eb8b3dbf48
BaconBoi95 <leeland230.fun@gmail.com>
2019-09-05 08:50:30 -0400

    Initial commit

---
+++ text/maps/RedsHouse1F.asm
@@ -0,0 +1,41 @@
+_MomWakeUpText::
+	text "MOM: Right."
+	line "All boys leave"
+	cont "home some day."
+	cont "It said so on TV."
+
+para "<PLAYER>: No”
+line "NOT YET”
+
+	para "PROF.OAK, next"
+	line "door, is looking"
+	cont "for you."
+	done
+
+_MomHealText1::
+	text "MOM: <PLAYER>!"
+	line "You should take a"
+	cont "quick rest."
+	prompt
+
+_MomHealText2::
+	text "MOM: Oh good!"
+	line "You and your"
+	cont "#MON are"
+	cont "looking great!"
+	cont "Now get out."
+	done
+
+_StandByMeText::
+	text "There's a movie"
+	line "on TV. Four boys"
+	cont "are walking on"
+	cont "railroad tracks."
+   line "THEY DIED”
+
+	para "I better go too."
+	done
+
+_TVWrongSideText::
+	text "Oops, wrong side."
+	done
---
+++ text/oakspeech.asm
@@ -0,0 +1,55 @@
+_OakSpeechText1::
+	text "Turn Back Now”
+	line "Dont do it"
+	cont "THIS IS NOT”
+
+	para "THE GAME”
+	line "YOU”
+	cont "ARE LOOKING FOR”
+	prompt
+
+_OakSpeechText2A::
+	text "RUN”
+	line "YOU DO NOT”
+	cont "BELONG HERE"
+	cont "THIS IS NOT”
+
+_OakSpeechText2B::
+	text $51,"THE #MON”
+	line "GAME YOU"
+	cont "KNOW AND"
+	cont "LOVE NOW”
+
+	para "THIS IS”
+
+	para "#MON HELL”
+	line "PLEASE GO NOW” 
+	prompt
+
+_IntroducePlayerText::
+	text "First, what is"
+	line "your real name?"
+	prompt
+
+_IntroduceRivalText::
+	text "This is my grand-"
+	line "son. He's been"
+	cont "your enemy since"
+	cont "you were a baby."
+
+	para "...Erm, what is"
+	line "his name again?"
+	prompt
+
+_OakSpeechText3::
+	text "<PLAYER>!"
+
+	para "DESTROY THE CARD”
+   line "BURN THE GAME”
+	cont "YOU BEED TO RUN”
+
+	para "OR"
+	line "YOU"
+	line "WILL"
+	LINE "D I E"
+	done
